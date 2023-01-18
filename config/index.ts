5002750069160c71b969219f06d6a921d9404ed3
Etiennepp <etiennep09@hotmail.fr>
2023-01-18 04:08:25 +0100

    Fixed api URL

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
-export const API_URL = dev ? "http://localhost:3000/api/" : "https://your_deployment.server.com";
+export const API_URL = dev ? "http://localhost:3000/api/" : "https://todogo-nine.vercel.app/api/";
 export const COLLECTION_COLORS = ["#dc3429", "#7f3e8e", "#0099df", "#00b75a", "#fddc59", "#2d495f"];
